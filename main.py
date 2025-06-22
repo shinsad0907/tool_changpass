3c984cffeef28ee082518daaf55eecaaeff50b1a
shinsad0907 <shinnohope.0907@gmail.com>
2025-06-22 22:06:40 +0700

    update changepass

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.chrome.options import Options
 import random
 import string
 import requests
