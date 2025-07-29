67f221e573ad530bc9cf5017b18280135afd9a08
shinsad0907 <112173054+shinsad0907@users.noreply.github.com>
2025-07-29 21:50:23 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class Main:
         options.add_argument("--disable-infobars")
         options.add_argument("--start-maximized")
         options.add_argument("--window-size=375,812")
-
+        options.add_argument("--lang=pl-PL")
 
         # Fake User-Agent
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36")
